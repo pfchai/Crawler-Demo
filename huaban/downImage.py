1d2bd665b543090753e864bd6ad1db0c11e4111e
chai <861503125@qq.com>
2015-02-04 14:25:20 +0800

    refactor downImage.py

--- huaban/downImage.py
+++ huaban/downImage.py
@@ -5,35 +5,50 @@
 import requests
 import re
 
-def find_No(htmlPage, pins = None):
+class HuabanCrawler():
     """ """
-    if pins == None:
-        pins = []
-    prog = re.compile(r'pin_id":(\d)+')
-    for p in prog.finditer(htmlPage):
-        pin = p.group()
-        pins.append(pin[8:])
-    return pins
 
-def find_image(htmlPage, images = None):
-    """ """
-    if images == None:
-        images = []
-    prog = re.compile(r'"key":"(\w)*-(\w)*"')
-    for p in prog.finditer(htmlPage):
-        imgUrl = p.group()
-        images.append(imgUrl[7:-1]+"_fw658")
-    return images
+    def __init__(self):
+        """ """
+        self.homeUrl = "http://huaban.com/favorite/beauty/"
+        self.appPins = []
+        self.imageUrls = []
 
-def make_ajax_url(No):
-    return "http://huaban.com/favorite/beauty/?i5p998kw&max=" + No + "&limit=20&wfl=1"
+    def __load_homePage(self):
+        return requests.get(url = self.homeUrl).content
 
-def load_homePage():
-    homeUrl = "http://huaban.com/favorite/beauty/"
-    req = requests.get(url = homeUrl)
-    return req.content
+    def __make_ajax_url(self, No):
+        """ """
+        return self.homeUrl + "?i5p998kw&max=" + No + "&limit=20&wfl=1"
 
-def load_more(maxNo):
-    req = requests.get(url = make_ajax_url(maxNo))
-    return req.content
+    def __load_more(self, maxNo):
+        return requests.get(url = self.__make_ajax_url(maxNo)).content
 
+    def __process_data(self, htmlPage):
+        """ """
+        prog = re.compile(r'app\.page\["pins"\].*')
+        appPins = prog.findall(htmlPage)
+        # covert javascript null to None
+        null = None
+        result = eval(appPins[0][19:-1])
+        for i in result:
+            self.appPins.append(i['pin_id'])
+            self.imageUrls.append("http://img.hb.aicdn.com/" + i["file"]["key"] + "_fw658")
+
+    def __save_image(self, imageName, content):
+        with open(imageName+'.jpeg', 'wb') as fp:
+            fp.write(content)
+
+    def downImages(self):
+        """ """
+        self.__process_data(self.__load_homePage())
+        for key, url in zip(self.appPins, self.imageUrls):
+            print key, url
+            req = requests.get(url)
+            self.__save_image(str(key), req.content)
+
+
+
+if __name__ == '__main__':
+    hc = HuabanCrawler()
+    hc.downImages()
